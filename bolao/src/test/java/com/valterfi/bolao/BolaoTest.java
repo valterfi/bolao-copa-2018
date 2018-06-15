d1e46717085de96cb9ffbcab0d0192980a9bde59
Valterfi Oliveira <valterfi.oliveira@sefin.fortaleza.ce.gov.br>
2018-06-15 16:58:45 -0300

    resultados do dia 15/06/2018

--- bolao/src/test/java/com/valterfi/bolao/BolaoTest.java
+++ bolao/src/test/java/com/valterfi/bolao/BolaoTest.java
@@ -10,7 +10,7 @@ public class BolaoTest {
 	 * Russia vs Arabia Saudita
 	 */
 	@Test
-	public void calcularPontosRodada1Test() {
+	public void calcularPontosRodada1RussiaArabiaSauditaTest() {
 		
 		Placar correto = new Placar(5, 0);
 		int peso = 10;
@@ -23,6 +23,66 @@ public class BolaoTest {
 		calcularPontosPorJogador(correto, Placar.palpitar(2,1), peso, "Teixeira");
 		calcularPontosPorJogador(correto, Placar.palpitar(2,1), peso, "Robson");
 		calcularPontosPorJogador(correto, Placar.palpitar(0,1), peso, "Valterfi");
+		
+	}
+	
+	/**
+	 * Egito vs Uruguai
+	 */
+	@Test
+	public void calcularPontosRodada1EgitoUruguaiTest() {
+		
+		Placar correto = new Placar(0, 1);
+		int peso = 11;
+		
+		calcularPontosPorJogador(correto, Placar.palpitar(1,2), peso, "Oton");
+		calcularPontosPorJogador(correto, Placar.palpitar(1,2), peso, "Pedro Paiva");
+		calcularPontosPorJogador(correto, Placar.palpitar(1,3), peso, "Ed");
+		calcularPontosPorJogador(correto, Placar.palpitar(0,2), peso, "Nicolas");
+		calcularPontosPorJogador(correto, Placar.palpitar(1,2), peso, "Do Vale");
+		calcularPontosPorJogador(correto, Placar.palpitar(0,2), peso, "Teixeira");
+		calcularPontosPorJogador(correto, Placar.palpitar(2,0), peso, "Robson");
+		calcularPontosPorJogador(correto, Placar.palpitar(1,3), peso, "Valterfi");
+		
+	}
+	
+	/**
+	 * Marrocos vs Ira
+	 */
+	@Test
+	public void calcularPontosRodada1MarrocosIraTest() {
+		
+		Placar correto = new Placar(0, 1);
+		int peso = 11;
+		
+		calcularPontosPorJogador(correto, Placar.palpitar(0,0), peso, "Oton");
+		calcularPontosPorJogador(correto, Placar.palpitar(1,1), peso, "Pedro Paiva");
+		calcularPontosPorJogador(correto, Placar.palpitar(2,1), peso, "Ed");
+		calcularPontosPorJogador(correto, Placar.palpitar(1,0), peso, "Nicolas");
+		calcularPontosPorJogador(correto, Placar.palpitar(1,1), peso, "Do Vale");
+		calcularPontosPorJogador(correto, Placar.palpitar(1,1), peso, "Teixeira");
+		calcularPontosPorJogador(correto, Placar.palpitar(0,0), peso, "Robson");
+		calcularPontosPorJogador(correto, Placar.palpitar(2,1), peso, "Valterfi");
+		
+	}
+	
+	/**
+	 * Portugal vs Espanha
+	 */
+	@Test
+	public void calcularPontosRodada1PortugalEspanhaTest() {
+		
+		Placar correto = new Placar(3, 3);
+		int peso = 11;
+		
+		calcularPontosPorJogador(correto, Placar.palpitar(1,2), peso, "Oton");
+		calcularPontosPorJogador(correto, Placar.palpitar(1,1), peso, "Pedro Paiva");
+		calcularPontosPorJogador(correto, Placar.palpitar(0,1), peso, "Ed");
+		calcularPontosPorJogador(correto, Placar.palpitar(1,1), peso, "Nicolas");
+		calcularPontosPorJogador(correto, Placar.palpitar(1,0), peso, "Do Vale");
+		calcularPontosPorJogador(correto, Placar.palpitar(2,1), peso, "Teixeira");
+		calcularPontosPorJogador(correto, Placar.palpitar(3,1), peso, "Robson");
+		calcularPontosPorJogador(correto, Placar.palpitar(2,4), peso, "Valterfi");
 		
 	}
 
